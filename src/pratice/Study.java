317df3b862e95ba28dc769c4f04395008902b7f7
chkfr <chkfr@DESKTOP-EN0GBG1>
2021-09-30 02:04:09 +0900

    hi

--- src/pratice/Study.java
+++ src/pratice/Study.java
@@ -3,7 +3,7 @@ package pratice;
 public class Study {
 	
 	public static void main(String [] args) {
-		System.out.println("Hello! This is Practice");
+		System.out.println("Hello! This is Practice!");
 	}
 
 }
